41ee347a750706112eafb3f18948276cf15984c3
GeekGirl822 <84748172+GeekGirl822@users.noreply.github.com>
2022-04-23 10:41:17 -0500

    Update CovidPneumonia_app.py

--- CovidPneumonia_app.py
+++ CovidPneumonia_app.py
@@ -12,7 +12,7 @@ def import_and_predict(image_data, model):
     prediction = model.predict(img_reshape)
     return prediction
 
-model = tf.keras.models.load_model('CovidPneumonia_app.h5')
+model = tf.keras.models.load_model('DetectCovidPneumonia_FINAL.h5')
 
 st.write("""
          # ***Covid-related Pneumonia detector***
