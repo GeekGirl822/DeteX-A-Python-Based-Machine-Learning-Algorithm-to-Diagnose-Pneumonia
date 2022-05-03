763137dd9e31ce6d6d4f4100286eeac91bd41d2a
GeekGirl822 <84748172+GeekGirl822@users.noreply.github.com>
2022-05-02 21:12:14 -0500

    Add files via upload

---
+++ Pneumonia_app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import tensorflow as tf
+from PIL import Image, ImageOps
+import numpy as np
+st.set_option('deprecation.showfileUploaderEncoding', False)
+
+# @st.cache(suppress_st_warning=True,allow_output_mutation=True)
+def import_and_predict(image_data, model):
+    image = ImageOps.fit(image_data, (300,300),Image.ANTIALIAS)
+    image = image.convert('RGB')
+    image = np.asarray(image)
+    st.image(image, channels='RGB')
+    image = (image.astype(np.float32) / 255.0)
+    img_reshape = image[np.newaxis,...]
+    prediction = model.predict(img_reshape)
+    return prediction
+
+model = tf.keras.models.load_model('DetectPneumonia_FINAL.h5')
+
+st.write("""
+         # ***Pneumonia detector***
+         """
+         )
+
+st.write("This is a simple image classification web app to predict pneumonia through chest x-ray images.")
+
+file = st.file_uploader("Please upload an image(jpg) file", type=["jpg"])
+
+if file is None:
+    st.text("You haven't uploaded a jpg image file")
+else:
+    imageI = Image.open(file)
+    prediction = import_and_predict(imageI, model)
+    pred = prediction[0][0]
+    if(pred > 0.5):
+        st.write("""
+                 ## **Prediction:** You are Healthy. Great!!
+                 """
+                 )
+        st.snow()
+    else:
+        st.write("""
+                 ## **Prediction:** You have Pneumonia. Please consult a doctor as soon as possible.
+                 """
+                )
